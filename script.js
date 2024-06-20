1a0c2890b1b3a7178988daf022fac407a63bf219
Oleksandr <sasha.shadad@gmail.com>
2024-06-20 23:02:15 +0200

    amendment

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const searchInput = document.getElementById('searchInput');
     const dataContainer = document.getElementById('dataContainer');
     const items = dataContainer.getElementsByClassName('main_proposition_about_info');
+    let debounceTimer;
 
-    searchInput.addEventListener('keyup', function() {
+    const performSearch = () => {
         const filter = searchInput.value.toLowerCase();
-        Array.from(items).forEach(item => {
-            const text = item.textContent || item.innerText;
-            if (text.toLowerCase().includes(filter)) {
-                item.style.display = "";
-            } else {
-                item.style.display = "none";
-            }
-        });
+        
+        for (const item of items) {
+            const h2 = item.querySelector("h2")
+            const text = h2.textContent || h2.innerText;
+            const match = text.toLowerCase().includes(filter);
+            item.style.display = match ? "" : "none";
+        }
+    };
+
+    searchInput.addEventListener('input', function() {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(performSearch, 300);
     });
+
+ 
+    performSearch();
 });
+
 

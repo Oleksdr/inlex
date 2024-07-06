0b05c52a970b4465110892b38c595dd258018d9e
Oleksandr <sasha.shadad@gmail.com>
2024-07-06 22:09:10 +0200

    adaptive

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const openMenu = document.querySelector(".burger");
+const mobileMenu = document.querySelector(".menu_mobile");
+const devider_top = document.querySelector(".devider_top");
+const devider_midle = document.querySelector(".devider_midle");
+const devider_bottom = document.querySelector(".devider_bottom");
+const span_text = document.querySelector(".span_text");
+const span_content = document.querySelector(".span_content");
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const searchInput = document.getElementById('searchInput');
     const dataContainer = document.getElementById('dataContainer');
@@ -25,3 +36,20 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+openMenu.addEventListener("click",function(){
+    mobileMenu.classList.toggle("active")
+    if(mobileMenu.classList.contains("active")){
+        span_text.textContent = `Закрити`;
+        devider_top.style.transform = "rotate(49deg)";
+        devider_bottom.style.transform = "rotate(-49deg)";
+        devider_midle.style.display = "none";
+        span_content.style.gap = 0 + "px";
+    }
+    else{
+        span_text.textContent = `Меню`;
+        devider_top.style.transform = "rotate(0deg)";
+        devider_bottom.style.transform = "rotate(0deg)";
+        devider_midle.style.display = "block";
+        span_content.style.gap = 6 + "px"; 
+    }
+})
